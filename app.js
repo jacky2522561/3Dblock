021baccc84c252a88e718ba4f76a6d184a402b71
jacky2522561 <jacky2522561@gmail.com>
2022-12-02 19:21:59 +0800

    12/02-3

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const des = document.querySelector('.info h3');
 const circle = document.querySelector('.circle');
 
 
-container.addEventListener('touchstart',(e)=>{
+container.addEventListener('mousemove',(e)=>{
     let xAxis = (window.innerWidth/2-e.pageX)/25;
     let yAxis = (window.innerHeight/2-e.pageY)/25;
     card.style.transform=`rotateY(${xAxis}deg) rotateX(${yAxis}deg)`
